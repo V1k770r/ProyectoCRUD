0c2bb7b686553aa59603645fe7ce1f3ecd48fb2a
eduvic <testprueba066@gmail.com>
2023-11-22 12:55:28 -0500

    Proyecto Primera Version

---
+++ src/main/java/org/example/platzi/main/Main.java
@@ -0,0 +1,18 @@
+package org.example.platzi.main;
+
+import org.example.platzi.model.Employee;
+import org.example.platzi.repository.EmployeeRepository;
+import org.example.platzi.repository.Repository;
+import org.example.platzi.view.SwingApp;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args)  {
+
+        SwingApp app = new SwingApp();
+        app.setVisible(true);
+
+    }
+}
+
