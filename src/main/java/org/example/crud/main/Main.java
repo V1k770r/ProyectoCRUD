a9654fa9a093f548bceb3e27fbadc162afa8b8c6
Victor <testprueba066@gmail.com>
2024-03-12 19:45:43 -0500

    First Version

---
+++ src/main/java/org/example/crud/main/Main.java
@@ -0,0 +1,15 @@
+package org.example.crud.main;
+
+import org.example.crud.view.SwingApp;
+
+
+
+public class Main {
+    public static void main(String[] args)  {
+
+        SwingApp app = new SwingApp();
+        app.setVisible(true);
+
+    }
+}
+
